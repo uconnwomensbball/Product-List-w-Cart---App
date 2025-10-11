715443737b3aaf5c55261d62115c84033f9d4174
uconnwbball <jd5208675309@gmail.com>
2025-10-11 15:49:32 -0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import { dessertsData } from "./data.js"
+import { nanoid } from 'nanoid'
+
+export default function App(){
+
+//variables 
+const [cart, setCart] = React.useState([])
+const [numberOfItems, setNumberOfItems] = React.useState(cart.length)
+const [totalPrice, setTotalPrice] = React.useState(0)
+const [isModalDisplayed, setIsModalDisplayed] = React.useState(false)
+
+//add ids to dessert items 
+const mappedDessertsDataWithIDs = dessertsData.map(function(dessert){
+    return {id: nanoid(), ...dessert}
+})
+
+//maps over dessert items to display them on DOM
+const mappedDessertData = mappedDessertsDataWithIDs.map(function(dessert){
+    return (
+    <div key={dessert.id}>
+            <img className="dessert-img" src={dessert.image} />
+            <button className = "add-to-cart-btn" onClick={()=>addToCart(dessert.id)}>
+                <img src="/assets/icon-add-to-cart.svg" /><span className = "bold">Add to Cart</span>
+            </button>
+        <p className = "bold">{dessert.name}</p><p className="red-text-color">${dessert.price.toFixed(2)}</p>
+    </div>)
+    })
+
+//maps over selected desserts to display in cart and in modal
+const mappedSelectedDesserts = cart.map(function(dessert){
+    return (
+    <>
+        <div className="cart-dessert" key={dessert.id}>
+            <div>  
+                <p className="bold">{dessert.name}</p>
+                <p><span className ="red-text-color bold">1x</span> @ ${dessert.price.toFixed(2)}</p>
+                <p>placeholder - total price</p>   
+            </div>
+            <div> 
+                <button className="remove-btn" onClick={()=>removeFromCart(dessert.id)}><img src="./assets/icon-remove-item.svg"/></button>
+            </div>
+        </div>
+        <hr></hr>
+    </>)})
+
+//function - adds items to cart
+function addToCart(id){ 
+    const newItem = mappedDessertsDataWithIDs.find(item =>item.id === id)
+    setCart(prevItems =>[...prevItems, newItem])
+    setNumberOfItems(prevLength=>prevLength + 1)
+}
+
+//function - removes items from cart
+function removeFromCart(id){
+    const cartWithoutRemovedItem = cart.filter(item =>item.id != id)
+    setCart(cartWithoutRemovedItem)
+    setNumberOfItems(prevLength=>prevLength - 1)
+}
+
+// calculates total price 
+const dessertsTotalPrice = cart.reduce(function(accumulator, currentValue) {
+    console.log(accumulator + currentValue.price)
+  return accumulator + currentValue.price
+}, 0)
+
+//function - updates total price 
+ React.useEffect(()=>{
+        setTotalPrice(dessertsTotalPrice)
+    }, [cart])
+
+//function - confirms order (aka displays modal)
+function confirmOrder(){
+    console.log("order confirmed!")
+    setIsModalDisplayed(true)
+}
+
+//function - starts new order and clears out prior order
+function startNewOrder(){
+    console.log("Start new order!")
+    setIsModalDisplayed(false)
+    setCart([])
+    setNumberOfItems(0)
+}
+
+return (
+     <>
+        <h1>Desserts</h1>
+            <div className="main">
+                <div id="desserts" className="desserts-div">
+                    {mappedDessertData}
+                </div>
+                <div className="cart-div">
+                    <h2 className="red-text-color">Your Cart (<span>{numberOfItems}</span>)</h2>
+                    <div id="cart-content">
+                    {numberOfItems === 0? 
+                    <div className = "empty-cart-content-div">
+                        <img src="./assets/illustration-empty-cart.svg"/>
+                        <p>Your added items will appear here</p>
+                    </div>:
+                    <>
+                       {mappedSelectedDesserts}
+                        <div className="order-div">
+                            <p className="bold">Order Total</p> 
+                            <p className="bold">${totalPrice.toFixed(2)}</p>
+                        </div>
+                        <button className = "red-bg-color confirm-btn bold" onClick={confirmOrder}>Confirm Order</button>
+                    </> }   
+                </div>
+                </div>
+            </div>
+            {isModalDisplayed &&  
+            <div className="modal-div">
+                <img src="./assets/icon-order-confirmed.svg"/>
+                <h1>Order Confirmed</h1>
+                <p>We hope you enjoy your food!</p>
+                {mappedSelectedDesserts}
+                <button className = "red-bg-color start-new-order-btn bold" onClick={startNewOrder}>Start New Order</button>
+            </div>}
+        <footer>&copy;JDJD Code <i className="fa-solid fa-scale-balanced"></i></footer>
+    </>
+)}
---
+++ data.js
@@ -0,0 +1,48 @@
+
+export const dessertsData = [
+    {
+     name:  "Waffle with Berries", 
+     price: 6.50, 
+     image: "/desserts/image-waffle-desktop.jpg" 
+    }, 
+    {
+     name:  "Vanilla Bean Creme Brulee", 
+     price: 7.00, 
+     image: "/desserts/image-creme-brulee-desktop.jpg" 
+    }, 
+    {
+     name:  "Macaron Mix of Five", 
+     price: 8.00, 
+     image: "/desserts/image-macaron-desktop.jpg" 
+    }, 
+    {
+     name:  "Classic Tiramisu", 
+     price: 5.50, 
+     image: "/desserts/image-tiramisu-desktop.jpg" 
+    }, 
+    {
+     name:  "Pistachio Baklava", 
+     price: 4.00, 
+     image: "/desserts/image-baklava-desktop.jpg" 
+    }, 
+    {
+     name:  "Lemon Meringue Pie", 
+     price: 5.00, 
+     image: "/desserts/image-meringue-desktop.jpg" 
+    }, 
+    {
+     name:  "Red Velvet Cake", 
+     price: 4.50, 
+     image: "/desserts/image-cake-desktop.jpg" 
+    },
+    {
+     name:  "Salted Caramel Brownie", 
+     price: 5.50, 
+     image: "/desserts/image-brownie-desktop.jpg" 
+    },  
+    {
+     name:  "Vanilla Panna Cotta", 
+     price: 6.50, 
+     image: "/desserts/image-panna-cotta-desktop.jpg" 
+    }, 
+]
